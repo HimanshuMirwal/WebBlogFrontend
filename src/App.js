6f5dd8d7d900de16e17c36a2196e5e87de8a5ee5
himanshu <himanshumirwal@gmail.com>
2021-09-17 21:32:22 +0530

    1st before deploy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Main from "./Main";
 import FullDescription from "../src/modules/FullDescriptioSection/FullDescription";
-import AdminLogin from "./modules/AdminSection/AdminLogin/AdminLogin";
-import Modal from "../src/modules/AdminSection/AdminDashboard/Modal";
-import display from "../src/modules/AdminSection/AdminDashboard/DisplayRecords";
-import AdminDashboard from "./modules/AdminSection/AdminDashboard/AdminDashboard";
 import AboutInfo from "./modules/AboutSection/AboutInfo";
 
 
@@ -19,8 +15,6 @@ export default class App extends React.Component {
                     {/*<Route path="home" component={Home} />*/}
                     <Route path="/description" component={FullDescription} />
                     <Route path="/description:Value" component={FullDescription} />
-                    <Route path="/login" component={AdminLogin} />
-                    <Route path="/edit:id" component={Modal} />
                     {/* <Route path="/place" component={AdminDashboard} /> */}
                     <Route path="/title/:tittle" component={Main} />
                     <Route path="/subtitle/:tittle?/:subtittle" component={Main} />
